e0fcd3dadfbbaa4640b55ec761f480edde30ea83
Pai314 <pai314.eequoalmc2@gmail.com>
2026-06-15 14:14:49 +0900

    鈴木陽向: 第10回提出

--- step5_color_draw.c
+++ step5_color_draw.c
@@ -1,5 +1,5 @@
-// step5: color.csv を読んで raylib で円を描く（完成版）
-// コンパイル: gcc step5_color_draw.c -o step5 -lraylib -lm
+// step5: color.csv を読んで raylib で図形を描く（改造版）
+// コンパイル: gcc step5_color_draw.c -o step5 $(pkg-config --cflags --libs raylib) -lm
 // 実行:       ./step5
 
 #include <stdio.h>
